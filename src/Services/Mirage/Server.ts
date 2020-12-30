1ad1cade7753d0adaacf6b6130a5d58ae1d9c11c
EhsanShakil <ahsanshakeel1994@gmail.com>
2020-12-30 23:13:38 +0500

    Creating Server

--- src/Services/Mirage/Server.ts
+++ src/Services/Mirage/Server.ts
@@ -8,3 +8,38 @@ export const handleErrors = (error: any, message = 'An error ocurred') => {
     },
   });
 };
+
+export const setupServer = (env?: string): Server => {
+  return new Server({
+    environment: env ?? 'development',
+
+    models: {
+      entry: Model.extend({
+        diary: belongsTo(),
+      }),
+      diary: Model.extend({
+        entry: hasMany(),
+        user: belongsTo(),
+      }),
+      user: Model.extend({
+        diary: hasMany(),
+      }),
+    },
+
+    factories: {
+      user: Factory.extend({
+        username: 'test',
+        password: 'password',
+        email: 'test@email.com',
+      }),
+    },
+
+    seeds: (server): any => {
+      server.create('user');
+    },
+
+    routes(): void {
+      this.urlPrefix = 'https://diaries.app';
+    },
+  });
+};
