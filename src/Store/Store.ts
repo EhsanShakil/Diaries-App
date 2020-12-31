3a42320eb6d9a32b09a2da17a9e1d9ca65407e55
EhsanShakil <ahsanshakeel1994@gmail.com>
2020-12-31 23:20:14 +0500

    Creaitng Store and Reducer

--- src/Store/Store.ts
+++ src/Store/Store.ts
@@ -1,1 +1,11 @@
-import { configureStore } from "@reduxjs/toolkit";
+import { configureStore } from '@reduxjs/toolkit';
+import rootReducer from '../RootReducer/RootReducer';
+import { useDispatch } from 'react-redux';
+
+const store = configureStore({
+  reducer: rootReducer,
+});
+
+type AppDispatch = typeof store.dispatch;
+export const useAppDispatch = () => useDispatch<AppDispatch>();
+export default store;
--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import App from './App/App';
 import reportWebVitals from './reportWebVitals';
 import { setupServer } from './Services/Mirage/Server';
+import { Provider } from 'react-redux';
+import store from './Store/Store';
 
 if (process.env.NODE_ENV === 'development') {
   setupServer();
@@ -11,10 +13,13 @@ if (process.env.NODE_ENV === 'development') {
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>,
   document.getElementById('root')
 );
+
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
