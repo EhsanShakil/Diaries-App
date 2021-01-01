548b819a5b68ee53a52a4cbe674ee5399fa8939a
EhsanShakil <ahsanshakeel1994@gmail.com>
2021-01-01 21:33:59 +0500

    Creating Redux Slices

--- src/RootReducer/RootReducer.ts
+++ src/RootReducer/RootReducer.ts
@@ -1,8 +1,16 @@
 import { combineReducers } from "@reduxjs/toolkit";
-
+import authReducer from "../Features/Auth/AuthSlice";
+import userReducer from "../Features/Auth/UserSlice";
+import diariesReducer from "../Features/Diary/DiarySlice";
+import entriesReducer from "../Features/Entry/EntriesSlice";
+import editorReducer from "../Features/Entry/EditorSlice";
 
 const rootReducer = combineReducers({
-
+  auth: authReducer,
+  diaries: diariesReducer,
+  entries: entriesReducer,
+  user: userReducer,
+  editor: editorReducer,
 });
 
 export type RootState = ReturnType<typeof rootReducer>;
